59d278a13e3d991b49aceba36d323bed90ff29c3
hopemoh <hopemoh2020@gmail.com>
2024-01-18 14:11:40 +0100

    add google anaylitic code

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H7E7LKZB8T"></script>
+<script>
+  
+</script>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4643996002315789"
           crossOrigin="anonymous"
         ></script>
       </head>
+      
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-H7E7LKZB8T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-H7E7LKZB8T');
+</script>
       <body className={inter.className}>
         <Navbar />
         <ToastContainer />
