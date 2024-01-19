0f5261bb085fdd8b209cc148545961f79b8ed6f9
hopemoh <hopemoh2020@gmail.com>
2024-01-19 04:59:33 +0100

    comment about,policy and contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+// import React from "react";
 
-const contactPage = () => {
-  return (
-    <div className="md:w-3/4 mx-auto mt-32 p-5  ">
-      <h1 className="text-3xl font-bold mb-5">
-        We appreciate your interest visiting our Website!
-      </h1>
-      <p className="text-gray-600 leading-loose">
-        if you have any questions, need more information, or would simply like
-        to connect with us, we'd love to hear from you! Our team is here to
-        assist you with anything you might need.don't hesitate to reach out.
-        <br />
-        <a href="mailto:contact@muchreview.com" style={{ color: "blue" }}>
-          contact@muchreview.com
-        </a>
-      </p>
-    </div>
-  );
-};
+// const contactPage = () => {
+//   return (
+//     <div className="md:w-3/4 mx-auto mt-32 p-5  ">
+//       <h1 className="text-3xl font-bold mb-5">
+//         We appreciate your interest visiting our Website!
+//       </h1>
+//       <p className="text-gray-600 leading-loose">
+//         if you have any questions, need more information, or would simply like
+//         to connect with us, we'd love to hear from you! Our team is here to
+//         assist you with anything you might need.don't hesitate to reach out.
+//         <br />
+//         <a href="mailto:contact@muchreview.com" style={{ color: "blue" }}>
+//           contact@muchreview.com
+//         </a>
+//       </p>
+//     </div>
+//   );
+// };
 
-export default contactPage;
+// export default contactPage;
